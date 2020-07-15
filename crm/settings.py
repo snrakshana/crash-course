7f4fbab4e2e3ddef1efadcc8f9a3172517503f98
snrakshana <59222780+snrakshana@users.noreply.github.com>
2020-07-15 15:52:37 +0530

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
